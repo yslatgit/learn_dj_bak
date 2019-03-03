aee8eced800331f0229f7a4c3a91baf26db0b3b9
yslatgit <986725816@qq.com>
2019-03-03 14:00:55 +0800

    unicide码转义

--- 01/Scripts/learn_dj/learn/tests.py
+++ 01/Scripts/learn_dj/learn/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 import requests,json
 
-url = 'http://127.0.0.1:8000/learn/add_user/'
+url = 'http://127.0.0.1:8000/learn/select_all/'
 data = {'user':'松林','pwd':'12'}
-req = requests.post(url,data)
-print(req.text)
+
+# content--->返回二进制的数据
+req = requests.get(url).content
+#text--->返回str的数据
+# 转义unicode码decode('unicode-escape')
+print(req.decode('unicode-escape'))
